a8a03be8f14321f64f0e5e24bf87c05e68755bc2
Pau <pau.verd11@gmail.com>
2022-11-05 16:46:36 +0100

    Update product repository

--- src/Product/Domain/Repository/ProductRepository.php
+++ src/Product/Domain/Repository/ProductRepository.php
@@ -4,11 +4,11 @@ namespace App\Product\Domain\Repository;
 
 use App\Product\Domain\Product;
 use App\Product\Domain\ValueObjects\ProductSku;
+use App\Product\Domain\ValueObjects\ProductCategory;
 
 interface ProductRepository
 {
-    public function save(Product $product): void;
     public function getAll(): array;
     public function getBySku(ProductSku $sku): ?Product;
-    public function delete(ProductSku $sku): void;
+    public function getByProductCategory(ProductCategory $category): array;
 }
