531a74a2f89b88f2f3f2c542397e2b15ebcb86e9
Pau <pau.verd11@gmail.com>
2022-11-06 17:54:27 +0100

    ProductDiscount - MongoRepository

--- src/Product/Domain/Repository/ProductDiscountRepository.php
+++ src/Product/Domain/Repository/ProductDiscountRepository.php
@@ -2,7 +2,10 @@
 
 namespace App\Product\Domain\Repository;
 
+use App\Product\Domain\ProductDiscount;
+
 interface ProductDiscountRepository
 {
     public function getAll(): array;
+    public function save(ProductDiscount $productDiscount): void;
 }
---
+++ src/Product/Infraestructure/ProductDiscountMongoRepository.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Product\Infrastructure;
+
+use App\Product\Domain\Repository\ProductDiscountRepository;
+
+use App\Product\Domain\ProductDiscount;
+
+use App\Product\Domain\ValueObjects\ProductSku;
+use App\Product\Domain\ValueObjects\DiscountPercentage;
+use App\Product\Domain\ValueObjects\ProductCategory;
+
+use MongoDB\Client;
+
+final class ProductDiscountMongoRepository implements ProductDiscountRepository
+{
+
+    public function __construct()
+    {
+        $user = getenv('MONGO_USERNAME');
+        $pass = getenv('MONGO_PASSWORD');
+        $host = getenv('MONGO_HOST');
+        $db   = getenv('MONGO_DATABASE');
+
+        $this->mongo = new Client("mongodb://$user:$pass@$host");
+        $this->collection = $this->mongo
+            ->selectDatabase($db)
+            ->selectCollection('discounts');
+    }
+    
+    public function getAll(): array
+    {
+        $productDiscounts = $this->collection->find();
+        $discountList = [];
+        
+        if (!empty($productDiscounts)) {
+            foreach ($productDiscounts as $discount) {
+                $discountList[] = new ProductDiscount(
+                    new DiscountPercentage($discount['percentage']),
+                    new ProductSku($discount['sku']),
+                    new ProductCategory($discount['category'])
+                );
+            };
+        }
+        
+        return $discountList;
+    }
+
+    public function save(ProductDiscount $productDiscount): void
+    {
+        $this->collection->insertOne([
+            'sku' => $productDiscount->sku()->value(),
+            'percentage' => $productDiscount->name()->value(),
+            'category' => $productDiscount->category()->value(),
+        ]);
+    }
+}
