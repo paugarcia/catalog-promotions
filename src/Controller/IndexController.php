60e4bb277e778f49e2fd19824b3105a408fb1aa9
Pau <pau.verd11@gmail.com>
2022-11-06 17:56:06 +0100

    Upload initial products to mongo collection

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -18,14 +18,19 @@ class IndexController extends AbstractController
 {
     public function index() : Response
     {
-        $mongoRepo = new ProductMongoRepository();
-
-        $saveProductApplicationService = new SaveProductApplicationService($mongoRepo);
+        $productMongoRepo = new ProductMongoRepository();
+        
+        $saveProductApplicationService = new SaveProductApplicationService($productMongoRepo);
         $saveProductCommandHandler = new SaveProductCommandHandler($saveProductApplicationService);
 
         $saveProductCommandHandler->handle(new SaveProductCommand('000001', 'BV Lean leather ankle boots', 'boots', 89000));
+        $saveProductCommandHandler->handle(new SaveProductCommand('000002', 'BV Lean leather ankle boots', 'boots', 99000));
+        $saveProductCommandHandler->handle(new SaveProductCommand('000003', 'Ashlington leather ankle boots', 'boots', 89000));
+        $saveProductCommandHandler->handle(new SaveProductCommand('000004', 'Naima embellished suede sandals', 'sandals', 79500));
+        $saveProductCommandHandler->handle(new SaveProductCommand('000005', 'Nathane leather sneakers', 'sneakers', 59000));
 
-        $mongoRepo->getAll();
+        echo "<pre>";
+        var_dump($productMongoRepo->getAll());die;
 
         return new Response("Hola mundo");
     }
