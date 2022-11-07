efcb972d58aff0ffda3ada911482dcd7397ad48d
Pau <pau.verd11@gmail.com>
2022-11-07 19:01:10 +0100

    Preparing filterByCategory route

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,56 +2,30 @@
 
 namespace App\Controller;
 
+use App\Product\Domain\ValueObjects\ProductCategory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
+use Symfony\Component\HttpFoundation\Request;
 
-
-use App\Product\Application\Commands\SaveProduct\SaveProductApplicationService;
-use App\Product\Application\Commands\SaveProduct\SaveProductCommand;
-use App\Product\Application\Commands\SaveProduct\SaveProductCommandHandler;
-
-use App\Product\Application\Commands\SaveProductDiscount\SaveProductDiscountApplicationService;
-use App\Product\Application\Commands\SaveProductDiscount\SaveProductDiscountCommand;
-use App\Product\Application\Commands\SaveProductDiscount\SaveProductDiscountCommandHandler;
-
-use App\Product\Infrastructure\ProductMongoRepository;
-use App\Product\Infrastructure\ProductDiscountMongoRepository;
+use App\Product\Infrastructure\ProductInMemoryRepository;
 
 
 class IndexController extends AbstractController
 {
-    public function index() : Response
+    // http://localhost:33000/index?filterByCategory=boots (GET PRODUCTS FILTER BY CATEGORY)
+    // http://localhost:33000/index (GET ALL PRODUCTS)
+    public function index(Request $request) : Response
     {
-        $productMongoRepo = new ProductMongoRepository();
-        
-        $saveProductApplicationService = new SaveProductApplicationService($productMongoRepo);
-        $saveProductCommandHandler = new SaveProductCommandHandler($saveProductApplicationService);
+        $productInMemoryRepository = new ProductInMemoryRepository();
+        if($request->query->get('filterByCategory') !== null){
+            $productCategoryToFind = new ProductCategory($request->query->get('filterByCategory'));
+            $products = $productInMemoryRepository->getByProductCategory($productCategoryToFind);
+        }else{
+            $products = $productInMemoryRepository->getAll();
+        }
 
-        $saveProductCommandHandler->handle(new SaveProductCommand('000001', 'BV Lean leather ankle boots', 'boots', 89000));
-        $saveProductCommandHandler->handle(new SaveProductCommand('000002', 'BV Lean leather ankle boots', 'boots', 99000));
-        $saveProductCommandHandler->handle(new SaveProductCommand('000003', 'Ashlington leather ankle boots', 'boots', 89000));
-        $saveProductCommandHandler->handle(new SaveProductCommand('000004', 'Naima embellished suede sandals', 'sandals', 79500));
-        $saveProductCommandHandler->handle(new SaveProductCommand('000005', 'Nathane leather sneakers', 'sneakers', 59000));
-
-        
-        $productDiscountMongoRepo = new ProductDiscountMongoRepository();
-        $saveProductDiscountApplicationService = new SaveProductDiscountApplicationService($productDiscountMongoRepo);
-        $saveProductDiscountCommandHandler = new SaveProductDiscountCommandHandler($saveProductDiscountApplicationService);
-
-        $saveProductDiscountCommandHandler->handle(new SaveProductDiscountCommand(30, null, 'boots'));
-        $saveProductDiscountCommandHandler->handle(new SaveProductDiscountCommand(15, '000003', null));
-
-       
-        echo "<pre>";
-        var_dump($productMongoRepo->getAll());
-        
-        
-        var_dump("HOLA");
-        var_dump($productDiscountMongoRepo->getAll());
-
-        die;
-
-        return new Response("Hola mundo");
+        return new JsonResponse($products);
     }
 }
